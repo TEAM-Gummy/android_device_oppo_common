1b4ee6331c14b38b461ff1bb6a07096a8b38ce78
Roman Birg <roman@cyngn.com>
2014-07-14 14:55:56 -0700

    Gestures: fix torch toggle delay on boot
    Change-Id: I1b7c32d1f9f4c67d5d1bf23448ffac33af8c3335

--- configpanel/src/com/cyanogenmod/settings/device/KeyHandler.java
+++ configpanel/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -98,6 +98,7 @@ public class KeyHandler implements DeviceKeyHandler {
             case GESTURE_V_SCANCODE:
                 if (GummyHelpers.isTorchAvailable(mContext)) {
                     Intent torchIntent = new Intent(TorchConstants.ACTION_TOGGLE_STATE);
+                    torchIntent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
                     mContext.sendBroadcast(torchIntent);
                 }
                 break;
